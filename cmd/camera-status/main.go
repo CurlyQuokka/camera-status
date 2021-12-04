acbd09899f7f0e5378087094db75858fc56e6a9d
CurlyQuokka <pstrusiewiczsurmacki@gmail.com>
2021-12-04 22:31:35 +0100

    Moved webserver to separate object

--- cmd/camera-status/main.go
+++ cmd/camera-status/main.go
@@ -7,24 +7,17 @@ package main
 // 4. Try to restart daemon when files are not up to date
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/CurlyQuokka/camera-status/pkg/mailer"
-	"github.com/CurlyQuokka/camera-status/pkg/utils"
 	"github.com/CurlyQuokka/camera-status/pkg/watcher"
+	"github.com/CurlyQuokka/camera-status/pkg/webserver"
 )
 
 var watcherObj *watcher.Watcher
 
 const (
-	StatusOK  = "OK"
-	StatusBad = "BAD"
-
-	green = "#cce36f"
-	red   = "#e36f6f"
-
 	mailFrom = "@gmail.com"
 	mailTo   = "@gmail.com"
 	mailPass = ""
@@ -34,62 +27,26 @@ const (
 )
 
 func main() {
-	mailerObj := mailer.NewMailer(mailFrom, mailTo, mailPass, smtpHost, smtpPort)
-	watcherObj = watcher.NewDefaultWatcher(mailerObj)
+	var webServerObj *webserver.WebServer
 
 	var port string
-
 	if len(os.Args) < 2 {
 		port = ":80"
 	} else {
 		port = ":" + os.Args[1]
 	}
 
+	mailerObj := mailer.NewMailer(mailFrom, mailTo, mailPass, smtpHost, smtpPort)
+	watcherObj = watcher.NewDefaultWatcher(mailerObj)
+	webServerObj = webserver.NewWebServer(port, watcherObj)
+
 	finished := make(chan bool)
 
 	go watcherObj.Watch(finished)
 
-	http.HandleFunc("/", getData)
+	http.HandleFunc("/", webServerObj.GetData)
 
 	http.ListenAndServe(port, nil)
 
 	<-finished
-}
-
-func getData(w http.ResponseWriter, req *http.Request) {
-	latest, isDaemonActive, isUpToDate, isSpaceSufficient, space := watcherObj.CheckStatus()
-	writeHTML(isDaemonActive, isUpToDate, isSpaceSufficient, space, latest, w)
-}
-
-func writeHTML(isDaemonActive, isUpToDate, isSpaceSufficient bool, space float64, latest utils.FileList, w http.ResponseWriter) {
-	bgColor := red
-	status := StatusBad
-
-	if isDaemonActive && isUpToDate && isSpaceSufficient {
-		bgColor = green
-		status = StatusOK
-	}
-
-	fmt.Fprintf(w, "<body bgcolor = \"%s\">", bgColor)
-	fmt.Fprintf(w, "<h1>STATUS: %s</h1>", status)
-
-	if status == StatusBad {
-		if !isDaemonActive {
-			fmt.Fprintf(w, "<h2>DAEMON IS INACTIVE!</h2>")
-		}
-		if !isUpToDate {
-			fmt.Fprintf(w, "<h2>RECORDINGS ARE NOT UP TO DATE!</h2>")
-		}
-		if !isSpaceSufficient {
-			fmt.Fprintf(w, "<h2>RUNNING OUT OF SPACE!</h2>")
-		}
-	}
-
-	for _, f := range latest {
-		fmt.Fprintf(w, "%s - %.2f MB</br>", f.Name, f.Size)
-	}
-
-	fmt.Fprintf(w, "</br>")
-	fmt.Fprintf(w, "Free space: <b>%.2f%%</b></br>", space*100)
-	fmt.Fprintf(w, "</body>")
 }
--- pkg/webserver/webserver.go
+++ pkg/webserver/webserver.go
@@ -1,1 +1,67 @@
 package webserver
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/CurlyQuokka/camera-status/pkg/utils"
+	"github.com/CurlyQuokka/camera-status/pkg/watcher"
+)
+
+const (
+	statusOK  = "OK"
+	statusBad = "BAD"
+
+	green = "#cce36f"
+	red   = "#e36f6f"
+)
+
+type WebServer struct {
+	port  string
+	watch *watcher.Watcher
+}
+
+func NewWebServer(port string, w *watcher.Watcher) *WebServer {
+	return &WebServer{
+		port:  port,
+		watch: w,
+	}
+}
+
+func (ws *WebServer) GetData(w http.ResponseWriter, req *http.Request) {
+	latest, isDaemonActive, isUpToDate, isSpaceSufficient, space := ws.watch.CheckStatus()
+	writeHTML(isDaemonActive, isUpToDate, isSpaceSufficient, space, latest, w)
+}
+
+func writeHTML(isDaemonActive, isUpToDate, isSpaceSufficient bool, space float64, latest utils.FileList, w http.ResponseWriter) {
+	bgColor := red
+	status := statusBad
+
+	if isDaemonActive && isUpToDate && isSpaceSufficient {
+		bgColor = green
+		status = statusOK
+	}
+
+	fmt.Fprintf(w, "<body bgcolor = \"%s\">", bgColor)
+	fmt.Fprintf(w, "<h1>STATUS: %s</h1>", status)
+
+	if status == statusBad {
+		if !isDaemonActive {
+			fmt.Fprintf(w, "<h2>DAEMON IS INACTIVE!</h2>")
+		}
+		if !isUpToDate {
+			fmt.Fprintf(w, "<h2>RECORDINGS ARE NOT UP TO DATE!</h2>")
+		}
+		if !isSpaceSufficient {
+			fmt.Fprintf(w, "<h2>RUNNING OUT OF SPACE!</h2>")
+		}
+	}
+
+	for _, f := range latest {
+		fmt.Fprintf(w, "%s - %.2f MB</br>", f.Name, f.Size)
+	}
+
+	fmt.Fprintf(w, "</br>")
+	fmt.Fprintf(w, "Free space: <b>%.2f%%</b></br>", space*100)
+	fmt.Fprintf(w, "</body>")
+}
